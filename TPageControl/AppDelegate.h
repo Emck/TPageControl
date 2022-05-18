5c9e730da503aa00c01f35395ed66e7f45d6d05d
Emck <emck.do@gmail.com>
2022-05-18 14:43:53 +0800

    import init app

---
+++ TPageControl/AppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.h
+//  TPageControl
+//
+//  Created by Emck on 5/18/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ TPageControl/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TPageControl
+//
+//  Created by Emck on 5/18/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
